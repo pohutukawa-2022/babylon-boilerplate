74baca0cb277f4f35fc5d72aac637ec9014263df
Ford <Fordnicholasjones@gmail.com>
2022-09-22 16:15:20 +1200

    Create first wall

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,84 +1,38 @@
 import * as BABYLON from 'babylonjs'
 import 'babylonjs-loaders'
-import img from './../assets/textures/amiga.jpg'
+import img from './../assets/textures/bricks.jpg'
 import vertShader from './../shaders/shader.vert'
 import fragShader from './../shaders/shader.frag'
 
 export default class Game {
-  constructor(canvasId) {
+  constructor (canvasId) {
     this.canvas = document.getElementById(canvasId)
     this.engine = new BABYLON.Engine(this.canvas, true)
-    this.time = 0
   }
 
-  createScene() {
+  createScene () {
     this.scene = new BABYLON.Scene(this.engine)
 
-    this.camera = new BABYLON.FreeCamera(
-      'camera1',
-      new BABYLON.Vector3(0, 5, -10),
-      this.scene
-    )
+    this.camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), this.scene)
     this.camera.setTarget(BABYLON.Vector3.Zero())
     this.camera.attachControl(this.canvas, false)
-    this.light = new BABYLON.HemisphericLight(
-      'light1',
-      new BABYLON.Vector3(0, 1, 0),
-      this.scene
-    )
+    this.light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this.scene)
 
-    let sphere = BABYLON.MeshBuilder.CreateSphere(
-      'sphere',
-      { segments: 16, diameter: 2 },
-      this.scene
-    )
-    sphere.position.y = 1
+    let cube = BABYLON.MeshBuilder.CreateBox('cube', {width: 10, height: 5, depth: 0.5}, this.scene)
 
-    BABYLON.MeshBuilder.CreateGround(
-      'ground',
-      { width: 6, height: 6, subdivisions: 2 },
-      this.scene
-    )
+    BABYLON.MeshBuilder.CreateGround('ground',
+      {width: 20, height: 20, subdivisions: 2}, this.scene)
 
     BABYLON.Effect.ShadersStore['customVertexShader'] = vertShader
     BABYLON.Effect.ShadersStore['customFragmentShader'] = fragShader
+    const cubeMaterial = new BABYLON.StandardMaterial()
+    cubeMaterial.diffuseTexture = new BABYLON.Texture(img, this.scene)
 
-    const shaderMaterial = new BABYLON.ShaderMaterial(
-      'shader',
-      this.scene,
-      {
-        vertex: 'custom',
-        fragment: 'custom',
-      },
-      {
-        attributes: ['position', 'normal', 'uv'],
-        uniforms: [
-          'world',
-          'worldView',
-          'worldViewProjection',
-          'view',
-          'projection',
-        ],
-      }
-    )
-
-    const mainTexture = new BABYLON.Texture(img, this.scene)
-    shaderMaterial.setTexture('textureSampler', mainTexture)
-    shaderMaterial.setFloat('time', 0)
-    shaderMaterial.setVector3('cameraPosition', BABYLON.Vector3.Zero())
-    sphere.material = shaderMaterial
+    cube.material = cubeMaterial
   }
 
-  doRender() {
+  doRender () {
     this.engine.runRenderLoop(() => {
-      const shaderMaterial = this.scene.getMaterialByName('shader')
-      shaderMaterial.setFloat('time', this.time)
-      this.time += 0.02
-
-      shaderMaterial.setVector3(
-        'cameraPosition',
-        this.scene.activeCamera.position
-      )
       this.scene.render()
     })
 
