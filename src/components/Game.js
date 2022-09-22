3ae652d2a7d693a85a921b9f5550f20dddd54f47
ford-jones <fordnicholasjones@gmail.com>
2022-09-22 22:09:04 +1200

    Psuedocode light varieties - torch / spotlight / hemispheric

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,45 +3,89 @@ import 'babylonjs-loaders'
 import img from './../assets/textures/bricks.jpg'
 import vertShader from './../shaders/shader.vert'
 import fragShader from './../shaders/shader.frag'
+import { Color3 } from 'babylonjs'
 
 export default class Game {
-  constructor (canvasId) {
+  constructor(canvasId) {
     this.canvas = document.getElementById(canvasId)
     this.engine = new BABYLON.Engine(this.canvas, true)
   }
 
-  createScene () {
+  createScene() {
     this.scene = new BABYLON.Scene(this.engine)
 
-    this.camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), this.scene)
+    this.camera = new BABYLON.FreeCamera(
+      'camera1',
+      new BABYLON.Vector3(0, 5, -10),
+      this.scene
+    )
     this.camera.setTarget(BABYLON.Vector3.Zero())
     this.camera.attachControl(this.canvas, false)
-    this.light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this.scene)
+    /*--------LIGHT TESTING--------*/
 
-    let cubeOne = BABYLON.MeshBuilder.CreateBox('cubeOne', {width: 10, height: 1.5, depth: 0.2}, this.scene)
+    // Spot light (torch light)
+    // this.light = new BABYLON.SpotLight(
+    //   'light1',
+    //   new BABYLON.Vector3(0, 10, 0),
+    //   new BABYLON.Vector3(2, -2, 1),
+    //   Math.PI / 1,
+    //   20,
+    //   this.scene
+    // )
+
+    // Point light (lightbulb)
+    this.light = new BABYLON.PointLight(
+      'light1',
+      new BABYLON.Vector3(0, 2, 0),
+      this.scene
+    )
+
+    //  Hemispheric light (natural / ambient)
+    //  this.light = new BABYLON.HemisphericLight('Light1', new BABYLON.vector3(0, 1, 0), this.scene)
+
+    let cubeOne = BABYLON.MeshBuilder.CreateBox(
+      'cubeOne',
+      { width: 10, height: 1.5, depth: 0.2 },
+      this.scene
+    )
     cubeOne.position.x = 0
     cubeOne.position.y = 0.7
     cubeOne.position.z = 6
 
-    let cubeTwo = BABYLON.MeshBuilder.CreateBox('cubeTwo', {width: 10, height: 1.5, depth: 0.2}, this.scene)
+    let cubeTwo = BABYLON.MeshBuilder.CreateBox(
+      'cubeTwo',
+      { width: 10, height: 1.5, depth: 0.2 },
+      this.scene
+    )
     cubeTwo.position.x = 0
     cubeTwo.position.y = 0.7
     cubeTwo.position.z = -6
 
-    let cubeThree = BABYLON.MeshBuilder.CreateBox('cubeThree', {width: 12, height: 1.5, depth: 0.2}, this.scene)
+    let cubeThree = BABYLON.MeshBuilder.CreateBox(
+      'cubeThree',
+      { width: 12, height: 1.5, depth: 0.2 },
+      this.scene
+    )
     cubeThree.position.x = 4.9
     cubeThree.position.y = 0.7
     cubeThree.position.z = 0
     cubeThree.rotation.y = Math.PI / 2
 
-    let cubeFour = BABYLON.MeshBuilder.CreateBox('cubeFour', {width: 12, height: 1.5, depth: 0.2}, this.scene)
+    let cubeFour = BABYLON.MeshBuilder.CreateBox(
+      'cubeFour',
+      { width: 12, height: 1.5, depth: 0.2 },
+      this.scene
+    )
     cubeFour.position.x = -4.9
     cubeFour.position.y = 0.7
     cubeFour.position.z = 0
     cubeFour.rotation.y = Math.PI / 2
 
-    BABYLON.MeshBuilder.CreateGround('ground',
-      {width: 20, height: 20, subdivisions: 2}, this.scene)
+    BABYLON.MeshBuilder.CreateGround(
+      'ground',
+      { width: 20, height: 20, subdivisions: 2 },
+      this.scene
+    )
 
     BABYLON.Effect.ShadersStore['customVertexShader'] = vertShader
     BABYLON.Effect.ShadersStore['customFragmentShader'] = fragShader
@@ -54,7 +98,7 @@ export default class Game {
     cubeFour.material = brickMaterial
   }
 
-  doRender () {
+  doRender() {
     this.engine.runRenderLoop(() => {
       this.scene.render()
     })
