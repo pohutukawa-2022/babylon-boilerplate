e2de60358a9a4b1ea2997c45460170447cd3f7cb
ford-jones <fordnicholasjones@gmail.com>
2022-09-27 23:02:48 +1300

    work on health reduction algorithm and making health render after landing page

--- src/components/health.js
+++ src/components/health.js
@@ -1,3 +1,26 @@
 export default function health(game) {
+  // if (game.inMenu === true) {
+  //   document.getElementById('health').innerHTML = ''
+  // } else {
+  //   document.getElementById(
+  //     'health'
+  //   ).innerHTML = `HEALTH: ${game.player.health}`
+  // }
   document.getElementById('health').innerHTML = `HEALTH: ${game.player.health}`
+
+  let player = game.player
+  let boss = game.boss.body
+
+  console.log('player', player.position)
+  console.log('ghost', boss.absolutePosition)
+
+  setTimeout(() => {
+    if (
+      Math.floor(boss.absolutePosition.x) === Math.floor(player.position.x) &&
+      Math.floor(boss.absolutePosition.z) === Math.floor(player.position.z)
+    ) {
+      console.log('hit')
+      game.player.health - 10
+    }
+  }, 500)
 }
